22dc2b9db4570daf13343870a16725b7ac9521b7
opeeny <godfreyopeeny@gmail.com>
2020-10-06 15:11:43 +0300

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
   albumInfo;
-  constructor(private _productService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     return this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
